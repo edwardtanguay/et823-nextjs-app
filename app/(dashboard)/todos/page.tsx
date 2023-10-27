3b5d9cc4409cbea5813779416e7087f5f8eff18a
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-27 23:35:25 +0200

    showing data from sqlite

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -1,7 +1,14 @@
-const PageTodos = () => {
+import db from '@/utils/db';
+
+const getData = async () => {
+	return await db.todo.findMany({});
+}
+
+const PageTodos = async () => {
+	const todos = await getData()
 	return ( 
 		<>
-		<h2 className="text-2xl">Todos</h2>	
+			<h2 className="text-2xl">{todos.length} Todos</h2>	
 		<p>testing</p>
 		</>
 	 );
