311a0ddddd088bd8dd82f32c137fe2c7d8f9fcd8
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-28 00:50:27 +0200

    sqlite saving form

--- components/NewTodoForm.tsx
+++ components/NewTodoForm.tsx
@@ -1,10 +1,12 @@
+import { newTodo } from '@/utils/actions';
 
 const NewTodoForm = ({}) => {
 	return (
 		<>
 			<div>
-				<form>
-					<input type="text" className="border border-gray-600"/>
+				<form action={newTodo}>
+					<input type="text" name="content" className="border border-gray-600"/>
+					<button type="submit">new todo</button>
 				</form>
 		</div>
 
