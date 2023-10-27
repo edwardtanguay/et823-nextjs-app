b10b162319f90edc17fa9d2d0fff7fcfc4b71dd0
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-27 17:03:49 +0200

    menu moved to main site header

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,27 +1,7 @@
-import Link from "next/link";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
-	const links = [
-		{
-			href: '/', title: 'Home'
-		},
-		{
-			href: '/todos', title: 'Todos'
-		},
-		{
-			href: '/docs', title: 'Docs'
-		},
-		{
-			href: '/products', title: 'Products'
-		}
-	]
 	return (
 		<div className="bg-slate-500 h-screen">
-			<ul className="flex gap-3 bg-slate-700 text-gray-400 p-3">
-				{links.map((link, i) => (
-					<li key={i}><Link href={link.href}>{link.title} </Link></li>
-				))}
-			</ul>
 			<div>dashboard header</div>
 			{children}
 			<div>dashboard footer</div>
